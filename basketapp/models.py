b7bd57df79794c7307c1c5e7a57be9e7c6ad679e
Alexandr_Ganikhin <alex.korpus@yande.ru>
2021-02-06 13:48:30 +0300

    @cached_property added in basket.models

--- basketapp/models.py
+++ basketapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from authapp.models import User
 
 from mainapp.models import Product
+from django.utils.functional import cached_property
 
 
 #ОБРАБОТКА УДАЛЕНИЯ КОРЗИНЫ ВАРИАНТ 1
@@ -31,15 +32,21 @@ class Basket(models.Model):
     def __str__(self):
         return f'Корзина для пользователя {self.user.username}, товар {self.product.name}'
 
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
     def sum(self):
         return self.quantity * self.product.price
 
     def total_quantity(self):
-        baskets = Basket.objects.filter(user=self.user)
+        #baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_cached
         return sum(basket.quantity for basket in baskets)
 
     def total_sum(self):
-        baskets = Basket.objects.filter(user=self.user)
+        # baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_cached
         return sum(basket.sum() for basket in baskets)
 
     @staticmethod
